de5b6fe228703ab8916d72a6c5bb424f1f9d7d9a
Hitesh <hiteshgupta8851@gmail.com>
2021-04-30 16:23:00 +0530

    socket for message and location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,29 @@
-io()
+const socket = io();
+
+socket.on("message", (text) => {
+  console.log(text);
+});
+
+const form = document.querySelector("#a");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = e.target.elements.t1.value;
+
+  //   console.log("form submitted!", message);
+  socket.emit("formSubmit", message);
+});
+
+document.querySelector("#send-location").addEventListener("click", (e) => {
+  if (!navigator.geolocation) {
+    return alert("GeoLocation not supported for your browser!");
+  }
+  navigator.geolocation.getCurrentPosition((position) => {
+    // console.log(position);
+    socket.emit("location", {
+      longitude: position.coords.longitude,
+      latitude: position.coords.latitude,
+    });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
-const http = require('http')
-const path = require('path')
-const express = require('express')
-const socketio = require('socket.io')
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const publicDirPath = path.join(__dirname, '../public')
+const publicDirPath = path.join(__dirname, "../public/");
 app.use(express.static(publicDirPath));
 
-io.on('connection', () => {
-    console.log('New Websocket Connection!');
-})
+let count = 0;
+
+const text = "welcome!";
+
+io.on("connection", (socket) => {
+  console.log("new web socket connection");
+
+  socket.emit("message", text); // send pointer to the message event, to chat.js
+
+  socket.broadcast.emit("message", "A New User Has Joined!");
+
+  // receive pointer to formSubmit from chat.js
+  socket.on("formSubmit", (message) => {
+    io.emit("message", message); // io => send message pointer to all connections
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A User Has Left!");
+  });
+
+  socket.on("location", (coords) => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    );
+  });
+});
 
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}`)
-})
+  console.log(`server is up on port ${port}`);
+});
